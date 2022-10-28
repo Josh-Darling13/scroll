aa8f081d6136877433f399a4839713328eb7007c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-28 18:44:28 -0400

    updated Javascript code for API

--- script.js
+++ script.js
@@ -4,6 +4,25 @@
 
 
 // Unsplash API
-const userName = "";
-const psswrd = "";
-const apiURL = `https://api.unsplash.com/photos/?`;
+const count = 10;
+const apiKey = "Bc5E6X6RWj8_vk7UlthqhdGIvgmsu8dO0yJq22RGF7Y";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// API get photos
+
+async function getPhotos(){
+
+
+    try{
+        const response = await fetch(apiURL);
+        const apiData = await response.json();
+        console.log(apiData);
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+// call to API function
+
+getPhotos();
