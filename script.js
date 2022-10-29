63bb111546e75bbda530bec83b052abfe659e68f
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-29 10:57:16 -0400

    added api out put code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const imageContainer = document.getElementById('imageContainer');
 const loader = document.getElementById('loader');
 
-let photosArray = [];
 
 
 // Unsplash API
@@ -9,20 +8,38 @@ const count = 10;
 const apiKey = "Bc5E6X6RWj8_vk7UlthqhdGIvgmsu8dO0yJq22RGF7Y";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+let photosArray = ['some value'];
+
 // API get photos
-
-
 async function getPhotos(){
 
     try{
         const response = await fetch(apiURL);
         photosArray = await response.json();
         console.log(photosArray);
+        displayPhotos();
 
     } catch(error){
         console.log(error);
     }
 }
+
+function displayPhotos(){
+    // create elements to display links and photos.in DOM
+    // create <a> to link to Unsplash
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+    // create <img> for photos
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+    // put <img> inside <a> inside imageContainer div
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    } );
+    }
 
 // call to API function
 
