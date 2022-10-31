d253e45a9cc271f57d67306551d2963026e7457e
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-31 15:12:53 -0400

    removed all commented out code

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ let photosArray = ['some value'];
 function setAttributes(element, attributes){
     for (const key in attributes){
         element.setAttribute(key, attributes[key]);
-        // LEFT OFF HERE
     }
 }
 
@@ -22,8 +21,7 @@ function displayPhotos(){
     // create <a> to link to Unsplash
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
+
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
@@ -31,8 +29,6 @@ function displayPhotos(){
 
     // create <img> for photos
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('alt', photo.alt_description);
 
         setAttributes(img, {
             src: photo.urls.regular,
