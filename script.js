825e3b93ed04f128965ccd08f4cb8945e7e050e5
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-31 13:20:40 -0400

    create helper function

--- script.js
+++ script.js
@@ -22,12 +22,24 @@ function displayPhotos(){
     // create <a> to link to Unsplash
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
     // create <img> for photos
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
+
     // put <img> inside <a> inside imageContainer div
         item.appendChild(img);
         imageContainer.appendChild(item);
