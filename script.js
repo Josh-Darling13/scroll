3bb33481c50090a29323abcd6343d5867af04e7c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-11-02 20:35:54 -0400

    vars to track image loading

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 let photosArray = ['some value'];
 
 // Unsplash API
